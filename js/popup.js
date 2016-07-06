f2b843bfcbf315a5df32f8be823e7b0aea6bd01d
Famous <FamousLastWordsZN@gmail.com>
2016-07-05 20:28:25 -0400

    some changes necessary

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,4 @@
 $(document).foundation();
 $(document).ready(function(){$('#shopNote').foundation('reveal', 'open')});
+
+ $(document).ready(function(){$('#thank-you').foundation('reveal', 'open')});
